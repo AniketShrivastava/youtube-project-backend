b26a0a022c2e8e05852ab0b89eeecd75036bb8ea
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-08-08 00:00:11 +0530

    add subscription routes

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription,
+} from "../controllers/subscription.controller.js"
+import { verifyJwt } from '../middlewares/auth.middleware.js';
+
+
+const router = Router();
+router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/toggle/:channelId")
+    .get(getSubscribedChannels)
+    .post(toggleSubscription);
+
+router.route("/toggle/:subscriberId").get(getUserChannelSubscribers);
+
+export default router
