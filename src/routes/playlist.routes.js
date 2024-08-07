df3c72ca995fcd126fe066dd235b0012cf7c1fe1
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-08-07 23:19:11 +0530

    add routes removeVideoFromPlaylist,     deletePlaylist,     updatePlaylist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,15 +1,27 @@
 import {Router} from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addVideoToPlaylist, createPlaylist, getPlaylistById, getUserPlaylists,  } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist,  } from "../controllers/playlist.controller.js";
 
 
 const router = Router()
+
 router.use(verifyJwt)
 
+
 router.route("/create").post(createPlaylist);
-router.route("/:userId").get(getUserPlaylists);
-router.route("/:playlistId").get(getPlaylistById);
+
+router.route("/:playlistId")
+    .get(getPlaylistById)
+    .put(updatePlaylist)
+    .delete(deletePlaylist);
+
+
+
+router.route("/:userId").get(getUserPlaylists)
 router.route("/:playlistId/videos/:videoId").post(addVideoToPlaylist);
+// Route to remove a video from a playlist
+router.route('/:playlistId/videos/:videoId').delete(removeVideoFromPlaylist);
+
 
 
 
